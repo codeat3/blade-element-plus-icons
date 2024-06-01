1d15a94b4065d02903f5c5cff51ce1d5c7f51175
Swapnil Sarwe <swapnilsarwe@gmail.com>
2024-06-02 01:16:20 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,92 @@
+<p align="center">
+    <img src="./socialcard-blade-element-plus-icons.png" width="1280" title="Social Card Blade Element Plus Icons">
+</p>
+
+# Blade Element Plus Icons
+
+<a href="https://github.com/codeat3/blade-element-plus-icons/actions?query=workflow%3ATests">
+    <img src="https://github.com/codeat3/blade-element-plus-icons/workflows/Tests/badge.svg" alt="Tests">
+</a>
+<a href="https://packagist.org/packages/codeat3/blade-element-plus-icons">
+    <img src="https://img.shields.io/packagist/v/codeat3/blade-element-plus-icons" alt="Latest Stable Version">
+</a>
+<a href="https://packagist.org/packages/codeat3/blade-element-plus-icons">
+    <img src="https://img.shields.io/packagist/dt/codeat3/blade-element-plus-icons" alt="Total Downloads">
+</a>
+
+A package to easily make use of [Element Plus Icons](https://github.com/element-plus/element-plus-icons) in your Laravel Blade views.
+
+For a full list of available icons see [the SVG directory](resources/svg) or preview them at [preview.gravity-ui.com/icons](https://preview.gravity-ui.com/icons/).
+
+## Requirements
+
+- PHP 7.4 or higher
+- Laravel 8.0 or higher
+
+## Installation
+
+```bash
+composer require codeat3/blade-element-plus-icons
+```
+
+## Updating
+
+Please refer to [`the upgrade guide`](UPGRADE.md) when updating the library.
+
+## Blade Icons
+
+Blade Element Plus Icons uses Blade Icons under the hood. Please refer to [the Blade Icons readme](https://github.com/blade-ui-kit/blade-icons) for additional functionality. We also recommend to [enable icon caching](https://github.com/blade-ui-kit/blade-icons#caching) with this library.
+
+## Configuration
+
+Blade Element Plus Icons also offers the ability to use features from Blade Icons like default classes, default attributes, etc. If you'd like to configure these, publish the `blade-element-plus-icons.php` config file:
+
+```bash
+php artisan vendor:publish --tag=blade-element-plus-icons-config
+```
+
+## Usage
+
+Icons can be used as self-closing Blade components which will be compiled to SVG icons:
+
+```blade
+<x-elemplus-back/>
+```
+
+You can also pass classes to your icon components:
+
+```blade
+<x-elemplus-back class="w-6 h-6 text-gray-500"/>
+```
+
+And even use inline styles:
+
+```blade
+<x-elemplus-back style="color: #555"/>
+```
+
+### Raw SVG Icons
+
+If you want to use the raw SVG icons as assets, you can publish them using:
+
+```bash
+php artisan vendor:publish --tag=blade-element-plus --force
+```
+
+Then use them in your views like:
+
+```blade
+<img src="{{ asset('vendor/blade-element-plus/back.svg') }}" width="10" height="10"/>
+```
+
+## Changelog
+
+Check out the [CHANGELOG](CHANGELOG.md) in this repository for all the recent changes.
+
+## Maintainers
+
+Blade Element Plus Icons is developed and maintained by [Swapnil Sarwe](https://swapnilsarwe.com).
+
+## License
+
+Blade Element Plus Icons is open-sourced software licensed under [the MIT license](LICENSE.md).
---
+++ tests/CompilesIconsTest.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use Orchestra\Testbench\TestCase;
+use Illuminate\Support\Facades\Config;
+use BladeUI\Icons\BladeIconsServiceProvider;
+use Codeat3\BladeElementPlusIcons\BladeElementPlusIconsServiceProvider;
+
+class CompilesIconsTest extends TestCase
+{
+    /** @test */
+    public function it_compiles_a_single_anonymous_component()
+    {
+        $result = svg('elemplus-back')->toHtml();
+
+        // Note: the empty class here seems to be a Blade components bug.
+        $expected = <<<'SVG'
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" fill="currentColor"><path fill="currentColor" d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64"/><path fill="currentColor" d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312z"/></svg>
+            SVG;
+
+        $this->assertSame($expected, $result);
+    }
+
+    /** @test */
+    public function it_can_add_classes_to_icons()
+    {
+        $result = svg('elemplus-back', 'w-6 h-6 text-gray-500')->toHtml();
+
+        $expected = <<<'SVG'
+            <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" fill="currentColor"><path fill="currentColor" d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64"/><path fill="currentColor" d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312z"/></svg>
+            SVG;
+
+        $this->assertSame($expected, $result);
+    }
+
+    /** @test */
+    public function it_can_add_styles_to_icons()
+    {
+        $result = svg('elemplus-back', ['style' => 'color: #555'])->toHtml();
+
+        $expected = <<<'SVG'
+            <svg style="color: #555" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" fill="currentColor"><path fill="currentColor" d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64"/><path fill="currentColor" d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312z"/></svg>
+            SVG;
+
+        $this->assertSame($expected, $result);
+    }
+
+    /** @test */
+    public function it_can_add_default_class_from_config()
+    {
+        Config::set('blade-element-plus-icons.class', 'awesome');
+
+        $result = svg('elemplus-back')->toHtml();
+
+        $expected = <<<'SVG'
+            <svg class="awesome" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" fill="currentColor"><path fill="currentColor" d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64"/><path fill="currentColor" d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312z"/></svg>
+            SVG;
+
+        $this->assertSame($expected, $result);
+    }
+
+    /** @test */
+    public function it_can_merge_default_class_from_config()
+    {
+        Config::set('blade-element-plus-icons.class', 'awesome');
+
+        $result = svg('elemplus-back', 'w-6 h-6')->toHtml();
+
+        $expected = <<<'SVG'
+            <svg class="awesome w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" fill="currentColor"><path fill="currentColor" d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64"/><path fill="currentColor" d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312z"/></svg>
+            SVG;
+
+        $this->assertSame($expected, $result);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            BladeIconsServiceProvider::class,
+            BladeElementPlusIconsServiceProvider::class,
+        ];
+    }
+}
